282f78e35f0d6fd187ce3ee4b8444215ecca46d1
Christian Pranzl <christian.pranzl@gmail.com>
2017-12-16 22:26:55 +0100

    Removed outdated bashrc stuff

--- git-completion/update.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.dotfiles/git-completion/git-prompt.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.dotfiles/git-completion/git-completion.bash
-chmod +x ~/.dotfiles/git-completion/git-prompt.sh
-chmod +x ~/.dotfiles/git-completion/git-completion.bash
-
